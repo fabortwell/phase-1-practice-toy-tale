d3c0fa13fafd16fa17a27c8291f3e7376e4c0868
Brian Sitati <briansit93@gmail.com>
2022-04-24 10:58:40 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -13,3 +13,18 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+headers:
+{
+  "Content-Type": "application/json",
+  Accept: "application/json"
+}
+
+body: JSON.stringify({
+  "name": "Jessie",
+  "image": "https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist",
+  "likes": 0
+})
+
+JSON.stringify({
+  "likes": newNumberOfLikes
+})
